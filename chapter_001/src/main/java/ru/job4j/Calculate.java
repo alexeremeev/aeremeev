e2a8ee04d755f1530bcb891be13e3bc74bbef8dc
Alexander Eremeev <eremeev@gmail.com>
2017-08-23 23:04:00 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,16 @@
 package ru.job4j;
 
+/**
+*  Class Класс для вывода строки в консоль
+* @author aeremeev;
+* @since 23.08.2017
+* @version 1
+*/
+
+/**
+* Вывод строки в консоль
+*/
+
 public class Calculate {
   public static void main (String[] args) {
     System.out.println("Hello World!");
