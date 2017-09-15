c671bf9b646670d83fb48cd80f996d9d1bd62df9
Alexander Eremeev <eremeev@gmail.com>
2017-09-15 15:15:05 +0300

    task 10022

---
+++ chapter_003/src/main/java/ru/job4j/testdrive/Benchmark.java
@@ -0,0 +1,65 @@
+package ru.job4j.testdrive;
+
+import java.util.List;
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * class Benchmark - тестрирование производительности коллеций.
+ * ArrayList, LinkedList, TreeSet тестируются на добавление и удаление N строк.
+ */
+public class Benchmark {
+    /**
+     * Метод добавления amount количества элементов в коллекцию.
+     * @param collection коллекция.
+     * @param amount колличество элементов.
+     * @return время выполнения операции в мс.
+     */
+    public long add(Collection<String> collection, int amount) {
+        long start = System.currentTimeMillis();
+
+        for (int index = 0; index != amount; index++) {
+            collection.add(Integer.toString(index));
+        }
+        return System.currentTimeMillis() - start;
+    }
+    /**
+     * Метод удаления amount количества элементов из коллекции.
+     * @param collection коллекция.
+     * @param amount колличество элементов.
+     * @return время выполнения операции в мс.
+     */
+    public long delete(Collection<String> collection, int amount) {
+        long start = System.currentTimeMillis();
+        for (int index = 0; index != amount; index++) {
+            collection.remove(Integer.toString(index));
+        }
+        return System.currentTimeMillis() - start;
+    }
+
+    /**
+     * Основной метод для программы, т.к. я изначально не знаю времени этих операций и не применяю JUnit.
+     * @param args аргументы.
+     */
+    public static void main(String[] args) {
+        Benchmark benchmark = new Benchmark();
+        List<String> arList = new ArrayList<>();
+        List<String> lkList = new LinkedList<>();
+        Set<String> tSet = new TreeSet<>();
+        System.out.format("Time to add 1000000 strings to ArrayList is: %s ms \n",
+                benchmark.add(arList, 1000000));
+        System.out.format("Time to add 1000000 strings to LinkedList is: %s ms \n",
+                benchmark.add(lkList, 1000000));
+        System.out.format("Time to add 1000000 strings to TreeSet is: %s ms \n",
+                benchmark.add(tSet, 1000000));
+        System.out.format("Time to remove 10000 strings from ArrayList is: %s ms \n",
+                benchmark.delete(arList, 10000));
+        System.out.format("Time to remove 10000 strings from LinkedList is: %s ms \n",
+                benchmark.delete(lkList, 10000));
+        System.out.format("Time to remove 10000 strings from TreeSet is: %s ms \n",
+                benchmark.delete(tSet, 10000));
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/testdrive/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for collections benchmarking task.
+ *
+ * @author Alexander Eremeev (mailto:eremeev@gmail.com).
+ * @version 1.
+ * @since 15.09.2017.
+ */
+package ru.job4j.testdrive;
