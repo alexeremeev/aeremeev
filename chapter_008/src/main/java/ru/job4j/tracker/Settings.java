d7fc0f8eefb848ee46f79d8bdf8fa9dcc096eafa
Alexander Eremeev <eremeev@gmail.com>
2017-11-09 13:08:21 +0300

    task 1734 "Трекер. NPE в меню убраны. Добавлены транзакции. Настройки убраны в .properties"

---
+++ chapter_008/src/main/java/ru/job4j/tracker/Settings.java
@@ -0,0 +1,38 @@
+package ru.job4j.tracker;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * class Settings - загрузчик настроек.
+ */
+public class Settings {
+    /**
+     * Properties.
+     */
+    private Properties properties = new Properties();
+
+    /**
+     * Констурктор.
+     * @param settingsFilename имя файла настроек.
+     */
+    public Settings(String settingsFilename) {
+        ClassLoader loader = Settings.class.getClassLoader();
+        try (InputStream stream = loader.getResourceAsStream(settingsFilename)) {
+            this.properties.load(stream);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    /**
+     * Получить значение настроек по ключу.
+     * @param key ключ.
+     * @return значение соответствующей настройки.
+     */
+    public String getSettings(String key) {
+        return this.properties.getProperty(key);
+    }
+
+}
